9163073a2c92ec2b68549c968b6ccfc74e76e9f6
Ricardo Henriques <paxcalpt@gmail.com>
2024-06-12 17:08:55 +0100

    Better makefile autogeneration that includes scripts calls

--- runSync.py
+++ runSync.py
@@ -45,6 +45,28 @@ def main():
             if include_file == "makefile":
                 src_text = src_text.replace("{{project_name}}", pyproject_config["tool"]["poetry"]["name"])
 
+                # find scripts and add them to the makefile
+                if "scripts" in pyproject_config["tool"]["poetry"]:
+                    scripts = pyproject_config["tool"]["poetry"]["scripts"]
+                    
+                    phony_script_lines = []
+                    echo_script_lines = []
+                    scripts_lines = []
+                    
+                    for script_name, script_cmd in scripts.items():
+                        phony_script_lines.append(f"{script_name}")
+                        echo_script_lines.append(f'@echo "  {script_name.ljust(27)}Run {script_cmd}"')
+                        scripts_lines.append(f"{script_name}:\n\tpoetry run {script_name}")
+
+                    src_text = src_text.replace("{{scripts_phony}}", "\n".join(phony_script_lines))
+                    src_text = src_text.replace("{{scripts_echo}}", "\n".join(echo_script_lines))
+                    src_text = src_text.replace("{{scripts}}", "\n\n".join(scripts_lines))
+
+                else:
+                    src_text = src_text.replace(" {{scripts_phony}}", "")
+                    src_text = src_text.replace("\n\t{{scripts_echo}}", "")
+                    src_text = src_text.replace("\n\n{{scripts}}", "")
+
             if not dest.exists() or src_text != dest_text:
                 dest.write_text(src_text)
                 print(f"Synced {include_file} to {i}")
