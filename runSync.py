71957358c6c918b4c4b5be59699e540473e6db76
Ricardo Henriques <paxcalpt@gmail.com>
2024-06-13 15:46:26 +0100

    chore: Refactor makefile to improve autogeneration and include scripts calls

--- runSync.py
+++ runSync.py
@@ -43,33 +43,39 @@ def main():
                 dest_text = dest.read_text()
 
             if include_file == "makefile":
-                src_text = src_text.replace("{{project_name}}", pyproject_config["tool"]["poetry"]["name"])
-
-                # find scripts and add them to the makefile
-                if "scripts" in pyproject_config["tool"]["poetry"]:
-                    scripts = pyproject_config["tool"]["poetry"]["scripts"]
-                    
-                    phony_script_lines = []
-                    echo_script_lines = []
-                    scripts_lines = []
-                    
-                    for script_name, script_cmd in scripts.items():
-                        phony_script_lines.append(f"{script_name}")
-                        echo_script_lines.append(f'@echo "  {script_name.ljust(27)}Run {script_cmd}"')
-                        scripts_lines.append(f"{script_name}:\n\tpoetry run {script_name}")
-
-                    src_text = src_text.replace("{{scripts_phony}}", "\n".join(phony_script_lines))
-                    src_text = src_text.replace("{{scripts_echo}}", "\n".join(echo_script_lines))
-                    src_text = src_text.replace("{{scripts}}", "\n\n".join(scripts_lines))
-
-                else:
-                    src_text = src_text.replace(" {{scripts_phony}}", "")
-                    src_text = src_text.replace("\n\t{{scripts_echo}}", "")
-                    src_text = src_text.replace("\n\n{{scripts}}", "")
+                src_text = generate_makefile(pyproject_config, src_text)
 
             if not dest.exists() or src_text != dest_text:
                 dest.write_text(src_text)
                 print(f"Synced {include_file} to {i}")
+
+def generate_makefile(pyproject_config, src_text) -> str:
+    src_text = src_text.replace("{{project_name}}", pyproject_config["tool"]["poetry"]["name"])
+
+    # find scripts and add them to the makefile
+    if "scripts" in pyproject_config["tool"]["poetry"]:
+        scripts = pyproject_config["tool"]["poetry"]["scripts"]
+        
+        phony_script_lines = []
+        echo_script_lines = []
+        scripts_lines = []
+        
+        for script_name, script_cmd in scripts.items():
+            phony_script_lines.append(f"{script_name}")
+            echo_script_lines.append(f'@echo "  {script_name.ljust(27)}Run {script_cmd}"')
+            scripts_lines.append(f"{script_name}:\n\tpoetry run {script_name}")
+
+        src_text = src_text.replace("{{scripts_phony}}", "\n".join(phony_script_lines))
+        src_text = src_text.replace("{{scripts_echo}}", "\n".join(echo_script_lines))
+        src_text = src_text.replace("{{scripts}}", "\n\n".join(scripts_lines))
+
+    else:
+        src_text = src_text.replace(" {{scripts_phony}}", "")
+        src_text = src_text.replace("\n\t{{scripts_echo}}", "")
+        src_text = src_text.replace("\n\n{{scripts}}", "")
+
+    return src_text
+
 
 if __name__ == "__main__":
     main()
